0dd754a8f47d882a54aa989f9cfa5d6801fb2c6d
Carlos Oliveira <carlos.sroliveira@hotmail.com>
2021-07-24 16:29:24 -0300

    init

--- public/assets/scripts.js
+++ public/assets/scripts.js
@@ -6,3 +6,4 @@ import {
   ColorsBackgroundTwo,
   ColorsBackgroundThree,
 } from "./background.js";
+import { slick } from "./slick.js";
---
+++ public/assets/slick.js
@@ -0,0 +1,8 @@
+export const slick = () => {
+  $(".carousel").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+};
+
+slick();
